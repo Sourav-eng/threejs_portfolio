0c915eb36ad67c9d6fb092329d9953530280ea69
Sourav <nishadsourav0@gmail.com>
2025-03-27 12:04:39 +0530

    ddg

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -1,25 +1,56 @@
 import React from 'react'
+import Globe from 'react-globe.gl'
 
 const About = () => {
     return (
         <section className='c-space my-20'>
-            <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 gap-5  h-full'>
+            <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 gap-5 h-full'>
                 <div className="col-span-1 xl:row-span-3">
                     <div className='grid-container'>
                         <img src="/assets/grid1.png" alt="grid-1" className='w-full sm:h-[276px] h-fit object-contain' />
                         <div>
                             <p className='grid-headtext'>Hello, I am Sourav</p>
-                            <p className='grid-subtext'>a passionate web developer with expertise in HTML, CSS, JavaScript, and backend technologies like Node.js. I enjoy building interactive 
-                                and user-friendly web applications,
-                                 from dynamic projects. </p>
+                            <p className='grid-subtext'>
+                                A passionate web developer with expertise in HTML, CSS, JavaScript, and backend technologies like Node.js. 
+                                I enjoy building interactive and user-friendly web applications.
+                            </p>
                         </div>
                     </div>
-
                 </div>
-                <div className='col-span-1 xl:row-span-3 ' >
-                    <div className='grid-container'>
-                        <img src="/assets/grid1.png" alt="grid-2" className='w-full h-fit object-contain sm:w-[276px]' />
 
+                <div className='col-span-1 xl:row-span-3'>
+                    <div className='grid-container'>
+                        <img src="/assets/grid2.png" alt="grid-2" className='w-full h-fit object-contain sm:w-[276px]' />
+                        <div>
+                            <p className='grid-headtext'>Tech Stack</p>
+                            <p className='grid-subtext'>
+                                I specialize in JavaScript with a focus on the React and Next.js ecosystem.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-span-1 xl:row-span-4'>
+                    <div className='grid-container'>
+                        <div className='rounded-3xl w-full h-fit flex sm:h-[326px] justify-center items-center'>
+                            <Globe 
+                                height={326} 
+                                width={326}
+                                showAtmosphere
+                                backgroundImageOpacity={0.5}
+                                globeImageUrl="//unpkg.com/three-globe/example/img/earth-nig.jpg"
+                                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                                backgroundColor="rgba(0,0,0,0)"
+                                labelData={[
+                                    {
+                                        lat: 40, lng: -100,
+                                        text: 'Hi I am here!',
+                                        color: 'white',
+                                        size: 20
+                                    }
+                                ]}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
